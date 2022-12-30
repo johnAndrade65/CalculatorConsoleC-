2cd13697125f0c683f6ce33b80f9e28535e3f01b
john65 <joaovini2667@gmail.com>
2022-12-30 10:32:39 -0300

    beta 0.1

---
+++ ConsoleCalculateCSharp/Metodos/Calculos.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ConsoleCalculateCSharp.Metodos {
+    internal class Calculos {
+        
+        //Metodo adição
+        public float Add(float x, float y) {
+            float resultado = x + y;
+            Console.WriteLine("= {0}", resultado);
+            return resultado;
+        }
+        //Metodo subtração
+        public float Subtracao(float x, float y) {
+            float resultado = x - y;
+            Console.WriteLine("= {0}", resultado);
+            return resultado;
+        }
+        //Metodo multiplicação
+        public float Multiplicar(float x, float y) {
+            float resultado = x * y;
+            Console.WriteLine("= {0}", resultado);
+            return resultado;
+        }
+        //Metodo divisão
+        public float Dividir(float x, float y) {
+            float resultado = x / y;
+            Console.WriteLine("= {0}",resultado);
+            return resultado;
+        }
+    }
+}
---
+++ ConsoleCalculateCSharp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ConsoleCalculateCSharp.Metodos;
+
+namespace ConsoleCalculateCSharp {
+    internal class Program {
+        static void Main(string[] args) {
+            //Variaveis numericas:
+            float num1 = 0;
+            float num2 = 0;
+
+            //Criação de objeto para acessar os metodos aritmeticos:
+            Calculos Operacao = new Calculos();
+
+            //Escolha uma operação:
+            Console.WriteLine("Qual operação deseja usar? (Digite apenas o icone!)");
+            Console.WriteLine(" + Somar  \r\n - Subtração  \r\n * Multiplicação \r\n / Divisão \r\n");
+
+            string operacao = Console.ReadLine();
+            Console.WriteLine("Operação selecionada {0} \r\n", operacao);
+
+            //Condicionais para saber se a operação é valida:
+            if (operacao == "+" || operacao == "-"|| operacao == "*" || operacao == "/") {
+                //Se for valida vai capturar os dois números
+                Console.WriteLine("Primeiro número:",operacao);
+                num1 = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("({0}) Segundo número:", operacao);
+                num2 = float.Parse(Console.ReadLine());
+            } else {
+                //Caso seja invalida vai exibir uma mensagem para escolher uma operação valida
+                Console.WriteLine("Escolha uma operação válida, tente novamente!");
+            }
+
+
+            //Condicionais para chamar o metodo/operação de acordo com a escolha do usuario:
+            if (operacao == "+") {
+                //Se operação for igual a "+" Soma os valores numericos
+                float resultado = Operacao.Add(num1, num2);
+            }else if (operacao == "-") {
+                //Se operação for igual a "-" Subtrai os valores numericos
+                float resultado = Operacao.Subtracao(num1, num2);
+            }else if (operacao == "*") {
+                //Se operação for igual a "*" Multiplica os valores numericos
+                float resultado = Operacao.Multiplicar(num1, num2);
+            }else if (operacao == "/") {
+                //Se operação for igual a "/" Dividi o primeiro valor pelo segundo
+                float resultado = Operacao.Dividir(num1, num2);
+            }
+        }
+    }
+}
